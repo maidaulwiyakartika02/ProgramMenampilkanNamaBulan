54818244b8508b0927b612f97fe521f4a4c7605e
maidaulwiyakartika02 <79822587+maidaulwiyakartika02@users.noreply.github.com>
2021-03-01 22:56:35 +0800

    Update Main.java

--- src/main/java/fosalgo/Main.java
+++ src/main/java/fosalgo/Main.java
@@ -12,9 +12,45 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         
-        //TULISKAN KODEMU DIBAWAH INI
-        
-        
+        System.out.println("PROGRAM PENAMPILAN NAMA BULAN");
+        int bulan;
+        System.out.println("masukan bulan");
+        bulan=masukan.nextint();
+        if (bulan==1)
+            System.out.println("Januari");
+        else if (bulan==2)
+                System.out.println("Februari");
+        else if (bulan==3)
+                System.out.println("Maret");
+        else if (bulan==4)
+                System.out.println("April");
+        else if (bulan==5)
+                System.out.println("Mei");
+        if (bulan==5)
+            System.out.println("Mei");
+        else if (bulan==6)
+                System.out.println("Juni");
+        else if (bulan==7)
+                System.out.println("Juli");
+        else if (bulan==8)
+                System.out.println("Agustus");
+        if (bulan==11)
+            System.out.println("Noverber");
+        else if (bulan==12)
+                System.out.println("Desember");
+        else if (bulan==1)
+                System.out.println("Januari");
+        else if (bulan==2)
+                System.out.println("Februari");
+        if (bulan==10)
+            System.out.println("Oktober");
+        else if (bulan==11)
+            System.out.println("Noverber");
+        else if (bulan==12)
+                System.out.println("Desember");
+        else if (bulan==1)
+                System.out.println("Januari");
+   
         
     }
 
